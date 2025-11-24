992efcdff15da4b281405b6b0a498bc50e33dbfa
MorvaiRoland <morvai11roland@gmail.com>
2025-11-24 21:50:36 +0100

    Auth change

--- src/auth.ts
+++ src/auth.ts
@@ -18,6 +18,7 @@ declare module "next-auth" {
 }
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  debug: true,
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
